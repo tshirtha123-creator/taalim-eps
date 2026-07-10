f85ccc1c34fb15a6e1beadf7781d1413ce6123a3
Hamza Jellam <tshirtha123@gmail.com>
2026-07-10 21:15:41 +0200

    Initial commit

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -13,7 +13,8 @@ const links = [
   "14fGBsMK1Wi", "18EgR9n461", "1EweJokmTn", "1bviSQiWE7", "18wGcovLLL", "18QsYcDnpH",
   "1CvoqwGYmV", "1D69X74jiv", "1D3jjMtuWc", "18tXRYsaih",
   "https://www.facebook.com/reel/1029270502981553",
-  "https://www.facebook.com/reel/1028595283087185"
+  "https://www.facebook.com/reel/1028595283087185",
+  "https://www.facebook.com/reel/3713196905486409"
 ];
 
 export const courses: Course[] = links.map((code, index) => {
